08d891d44a999744397598ec665bfb539226ed43
Vitalii Karasov <vitaliikarasov@Vitaliis-MacBook-Air.local>
2022-12-07 00:00:36 +0200

    fix:hotfix

--- src/http/index.ts
+++ src/http/index.ts
@@ -18,7 +18,7 @@
 import axios from 'axios';
 import {AuthResponse} from "../models/response/AuthResponse";
 
-export const API_URL = `https://post-app-api-production.up.railway.app`
+export const API_URL = `https://post-app-api-production.up.railway.app/api`
 
 const $api = axios.create({
     withCredentials: true,
